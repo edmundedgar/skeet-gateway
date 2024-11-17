ee158296b8ee45acbaf852b566f5be02b2e9e439
turbocrime <turbocrime@users.noreply.github.com>
2024-11-17 18:29:25 +0700

    minor refactor

--- bot/validate-inclusion.ts
+++ bot/validate-inclusion.ts
@@ -1,5 +1,5 @@
 import { Agent } from "@atproto/api";
-import { DidResolver } from "@atproto/identity";
+import { DidDocument, DidResolver } from "@atproto/identity";
 import { RepoRecord } from "@atproto/lexicon";
 import {
   BlockMap,
@@ -11,11 +11,20 @@ import {
 } from "@atproto/repo";
 import { CID } from "multiformats";
 import assert from "node:assert";
-import { VerificationMethod } from "./watcher.js";
 
-const [atUriArg] = process.argv.slice(2);
+// TODO:
+// - validate order
+// - validate depth
+// - don't return before whole tree is valid
+// - cycle detection
+// - more efficient walk?
+// - don't walk all paths in parallel?
 
-// "at://did:plc:mtq3e4mgt7wyjhhaniezej67/app.bsky.feed.post/3laydu3mgac2v";
+const [
+  atUriArg = "at://did:plc:mtq3e4mgt7wyjhhaniezej67/app.bsky.feed.post/3laydu3mgac2v",
+  agentArg = "https://bsky.network",
+  plcUrl = "https://plc.directory",
+] = process.argv.slice(2);
 
 const [_, did, collection, rkey] = atUriArg.match(
   /^at:\/\/(did:[\w\d]*:[\w\d]*)\/([\w\d\.]*)\/([\w\d]*)/
@@ -24,16 +33,15 @@ const [_, did, collection, rkey] = atUriArg.match(
 const debug = console.log;
 
 const td = new TextDecoder();
-const te = new TextEncoder();
 
 const parseNodeData = (r: RepoRecord) => nodeDataDef.schema.parse(r);
 const parseCommitData = (r: RepoRecord) => schema.commit.parse(r);
 
-export const validateInclusion = async (
-  verificationMethod: VerificationMethod,
+export async function validateInclusion(
+  verificationMethod: DidDocument["verificationMethod"][number],
   { did, collection, rkey }: { did: string; collection: string; rkey: string },
   { roots: [root], blocks }: Awaited<ReturnType<typeof readCar>>
-) => {
+) {
   assert(
     verificationMethod.controller === did,
     "Uncontrolled verification method"
@@ -55,16 +63,14 @@ export const validateInclusion = async (
   debug("root sig ok");
 
   return await walkTree(`${collection}/${rkey}`, rootRecord.data, blocks);
-};
+}
 
-const hex = (b: ArrayBufferLike) => Buffer.from(b).toString("hex");
-
-const walkTree = async (
+async function walkTree(
   targetKey: string,
   hereId: CID,
   blocks: BlockMap,
   depth = 0
-) => {
+) {
   const hereRecord = blocks.get(hereId);
 
   assert(hereRecord, `No record for ${hereId}`);
@@ -72,7 +78,7 @@ const walkTree = async (
 
   const hereSha = await crypto.subtle.digest("SHA-256", hereRecord);
   assert(
-    hex(hereSha) === hex(hereId.multihash.digest),
+    td.decode(hereSha) === td.decode(hereId.multihash.digest),
     "Failed to verify hash of record"
   );
   debug(depth, "valid sha", hereId);
@@ -94,7 +100,7 @@ const walkTree = async (
       debug(depth, "detected target value", v, targetKey);
       return assertValidTarget(targetKey, v, blocks);
     }
-    debug("passing non-target value", td.decode(k));
+    debug(depth, "passing non-target value", td.decode(k));
     if (t && blocks.has(t)) {
       debug(depth, "pushing right walk", t);
       walk.push(walkTree(targetKey, t, blocks, depth + 1));
@@ -104,43 +110,50 @@ const walkTree = async (
   debug(depth, "passed node", hereId);
   // throws on purpose if `walk` is empty
   return Promise.any(walk);
-};
+}
 
-const assertValidTarget = async (
-  targetKey: string,
-  v: CID,
-  blocks: BlockMap
-) => {
+async function assertValidTarget(targetKey: string, v: CID, blocks: BlockMap) {
   debug("validating target", targetKey, v);
   const targetRecord = blocks.get(v);
   const targetHash = await crypto.subtle.digest("SHA-256", targetRecord);
   assert(
-    hex(targetHash) === hex(v.multihash.digest),
+    td.decode(targetHash) === td.decode(v.multihash.digest),
     "Failed to verify hash of target record"
   );
   debug("target hash ok");
   return cborToLexRecord(targetRecord);
-};
+}
 
-// execution
+async function main(agent: Agent, directory: DidResolver) {
+  const fetchVm = async () => {
+    const didDoc = await directory.resolve(did);
+    assert(
+      didDoc.verificationMethod.length,
+      "No verification methods available"
+    );
+    return didDoc.verificationMethod[0];
+  };
 
-const {
-  verificationMethod: [vm],
-} = await new DidResolver({}).resolve(did);
-assert(vm, "No validation method");
+  const fetchCar = async () => {
+    const { success, headers, data } = await agent.com.atproto.sync.getRecord({
+      did,
+      rkey,
+      collection,
+    });
+    assert(success, `Failed to fetch record. ${headers}`);
+    return data;
+  };
 
-const syncRecordRequest = await new Agent(
-  "https://bsky.network"
-).com.atproto.sync.getRecord({
-  did,
-  rkey,
-  collection,
-});
+  const vm = await fetchVm().catch(() => fetchVm());
+  const car = await fetchCar().catch(() => fetchCar());
 
-const valid = await validateInclusion(
-  vm,
-  { did, collection, rkey },
-  await readCar(syncRecordRequest.data)
-);
+  const valid = await validateInclusion(
+    vm,
+    { did, collection, rkey },
+    await readCar(car)
+  );
 
-console.log({ valid });
+  console.log("Valid record:", valid);
+}
+
+main(new Agent(agentArg), new DidResolver({ plcUrl }));
