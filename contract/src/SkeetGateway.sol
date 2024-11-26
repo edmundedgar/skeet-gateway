144bc5e178d25527c7d29beeff08928df2ec8594
turbocrime <turbocrime@users.noreply.github.com>
2024-11-26 10:26:14 -0600

    do it

--- contract/src/SkeetGateway.sol
+++ contract/src/SkeetGateway.sol
@@ -62,8 +62,6 @@ contract SkeetGateway {
         (CommitCbor.Commit memory rootCommit,) = CommitCbor.readCommit(commit, 0);
         CidCbor.CidBytes32 rootCid = CidCbor.readCidBytes32(commit, rootCommit.data);
 
-        (TreeNodeCbor.TreeNode memory rootNode,) = TreeCbor.getCid(tree, rootCid);
-
         string memory key = string.concat(collection, "/", rkey);
 
         TreeCbor.verifyInclusion(tree, treeNodes, rootCid, target, key);
