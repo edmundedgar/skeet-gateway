ed301a2eb7e6aeaa0b17fdd5191ba7d172dcad4a
turbocrime <turbocrime@users.noreply.github.com>
2024-12-10 17:09:18 -0800

    format, snapshot, coverage

--- contract/src/SignerSafe.sol
+++ contract/src/SignerSafe.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 
 contract SignerSafe {
-
     address public owner;
 
     constructor() {
@@ -21,5 +20,4 @@ contract SignerSafe {
             success := call(not(0), to, value, add(data, 0x20), mload(data), 0, 0)
         }
     }
-
 }
