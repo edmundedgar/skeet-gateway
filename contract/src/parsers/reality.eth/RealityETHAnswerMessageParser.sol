1fca6148b7e6bc7040e019bc99264b5acadfe0eb
Edmund Edgar <ed@socialminds.jp>
2024-12-24 12:05:04 +0900

    fix comment

--- contract/src/parsers/reality.eth/RealityETHAnswerMessageParser.sol
+++ contract/src/parsers/reality.eth/RealityETHAnswerMessageParser.sol
@@ -20,8 +20,8 @@ contract RealityETHAnswerMessageParser is IMessageParser {
 
     constructor(address _realityETH, string memory _linkURLPrefix) {
         realityETH = _realityETH;
-        // CBOR-encoded bytes for eg:
-        // <header>"https://reality.eth.link/app/#!/network/11155111/contract/0xaf33dcb6e8c5c4d9ddf579f53031b514d19449ca/token/ETH/question/0xaf33dcb6e8c5c4d9ddf579f53031b514d19449ca-0x<64 characters which are then truncated>"
+        // URL eg
+        // https://reality.eth.link/app/#!/network/11155111/contract/0xaf33dcb6e8c5c4d9ddf579f53031b514d19449ca/token/ETH/question/0xaf33dcb6e8c5c4d9ddf579f53031b514d19449ca-0x<64 characters which are then truncated>"
         linkURLPrefix = _linkURLPrefix;
         require(realityETH != address(0), "missing realityETH address");
         require(bytes(linkURLPrefix).length > 0, "missing linkURLPrefix");
