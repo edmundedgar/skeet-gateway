676715a62d754862e5455c4cf5fcc7557157948c
Edmund Edgar <ed@socialminds.jp>
2024-12-27 09:20:09 +0900

    Store the bot metadata

--- python-tools/load_bots.py
+++ python-tools/load_bots.py
@@ -21,12 +21,28 @@ parser_config = {}
 start_block = w3.eth.block_number
 max_blocks = 100000
 block_number = start_block
+countLoaded = 0
 while block_number > start_block - max_blocks:
+    isFound = False
     block_number = block_number - 999 
     print("fetch from "+str(block_number))
     logs = gateway_contract.events.LogAddBot().get_logs(from_block=block_number, to_block = block_number+999)
     for log in logs:
        # AttributeDict({'args': AttributeDict({'parser': '0xA84F9FC27e849f636c18125c74358E003492f437', 'domain': 'unconsensus.com', 'subdomain': 'bbs'}), 'event': 'LogAddBot', 'logIndex': 125, 'transactionIndex': 101, 'transactionHash': HexBytes('0x215cdf64f66304cd3d8116ef85a8447eea814465003153970018ce117449fef1'), 'address ': '0xeB1Ef91e8658FE4b13Cc6A6d8E46c6Dcf5Aa63C1', 'blockHash': HexBytes('0x28353957873746df7ae5c56a9bd6143eb080bc4ead0330e6e37ede4894c1e6e8'), 'blockNumber': 7 351100})                       
-        parser_config[log['args']['subdomain'] + '.' + log['args']['domain']] = log['args']['parser']
+        metadata_arg = log['args']['metadata'];
+        metadata = None
+        if metadata_arg == "":
+            metadata = {}
+        else:
+            metadata = json.loads(metadata_arg)
+        parser_config[log['args']['subdomain'] + '.' + log['args']['domain']] = {
+            "parser": log['args']['parser'],
+            "metadata": metadata
+        }
+        countLoaded = countLoaded + 1
+        isFound = True
         with open('parser_config.json', 'w', encoding='utf-8') as f:
             json.dump(parser_config, f, ensure_ascii=False, indent=4)
+
+    if isFound:
+        print("loaded "+str(countLoaded))
