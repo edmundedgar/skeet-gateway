3eb05c510d2acc446b969118f9b1bcce830a494a
Edmund Edgar <ed@socialminds.jp>
2025-01-04 11:33:56 +0900

    base64 url encoding of sig working

--- contract/src/DidFormats.sol
+++ contract/src/DidFormats.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
 abstract contract DidFormats {
 
     function base32CidToSha256(string calldata cid) pure public returns (bytes32) {
         return bytes32(0);
+    }
+
+    function didKeyToBytes(string calldata key) pure public returns (address) {
+    
+        return address(0);
     }
 
     function didKeyToAddress(string calldata key) pure public returns (address) {
@@ -12,8 +19,7 @@ abstract contract DidFormats {
     }
 
     function sigToBase64URLEncoded(bytes calldata rs) pure public returns (bytes memory) {
-        bytes memory ret;
-        return ret;
+        return bytes(Base64.encodeURL(rs));
     }
 
 }
--- contract/test/DidFormats.sol
+++ contract/test/DidFormats.sol
@@ -16,10 +16,19 @@ contract DidVerifierTest is Test{
         client= new DidFormatClient();
     }
 
-    function testSigToBase64UrlEncoded() external view {
+    function testSigToBase64UrlEncoded() public view {
         string memory origEncodedSig = "2-G4D9YXFCB6LTrvalq23o7vey1W7KcSVDf-IkfU_x8AFAVUn2VtxSXTtCMr5tAe72KyPSzzw0kaV0M88JuCEA";
         bytes memory decodedSig = bytes(hex"dbe1b80fd61714207a2d3aef6a5ab6de8eef7b2d56eca7125437fe2247d4ff1f001405549f656dc525d3b4232be6d01eef62b23d2cf3c3491a57433cf09b8210");
         bytes memory encodedSig = client.sigToBase64URLEncoded(decodedSig);
-                                         
+        assertEq(keccak256(encodedSig), keccak256(bytes(origEncodedSig)), "sig should match after reencoding");
     }
+
+    function testDidKeyToBytes() public view {
+
+    }
+
+    //bytes32 hash = keccak256(publicKey);
+    //return address(uint160(uint256(hash)));
+
+
 }
