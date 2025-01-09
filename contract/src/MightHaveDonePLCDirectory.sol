72868799e57d422625909b67dcc2506dccc0e28d
Edmund Edgar <ed@socialminds.jp>
2025-01-09 14:16:18 +0900

    Add comments, hose out incomplete stuff

--- contract/src/MightHaveDonePLCDirectory.sol
+++ contract/src/MightHaveDonePLCDirectory.sol
@@ -35,30 +35,26 @@ contract MightHaveDonePLCDirectory is DidVerifier {
         bytes32 uncontroversialTip; // The tip of the chain, or 0x0 if the chain has forked
     }
 
-    struct ApprovalRule {
-        bytes32 fromUpdateOp;
-        uint256 untilTS;
-    }
-
-    enum UpdateEntry {
-        oneOfManyPossibleCids,
-        createdAt,
-        numRotationKeys,
-        rotationKeys,
-        numVerificationMethods,
-        verificationMethod
-    }
-
     mapping(bytes32 => Did) public dids;
 
+    /// @notice Whether the specified update is at the tip (ie has no children)
+    /// @param didGenesisHash The did
+    /// @param updateHash The update
+    /// @return True if the update is at the tip
     function isAtTip(bytes32 didGenesisHash, bytes32 updateHash) external view returns (bool) {
         return (dids[didGenesisHash].updates[updateHash].countChildren > 0);
     }
 
+    /// @notice The update at the tip of the chain, if it has not forked
+    /// @param didGenesisHash The did
+    /// @return The update hash if the chain has not forked, or 0x0 if it has
     function uncontroversialTip(bytes32 didGenesisHash) external view returns (bytes32) {
         return dids[didGenesisHash].uncontroversialTip;
     }
 
+    /// @notice The verification address of the update at the tip of the chain, if it has not forked
+    /// @param didGenesisHash The did
+    /// @return The verification address if the chain has not forked, or 0x0 if it has
     function uncontroversialVerificationAddress(bytes32 didGenesisHash) external view returns (address) {
         bytes32 tip = dids[didGenesisHash].uncontroversialTip;
         if (tip == bytes32(0)) {
@@ -67,17 +63,30 @@ contract MightHaveDonePLCDirectory is DidVerifier {
         return dids[didGenesisHash].updates[tip].verificationMethod;
     }
 
+    /// @notice Whether the specified update has a fork, ie has multiple children
+    /// @param didGenesisHash The did
+    /// @param updateHash The update at which you want to know the whether there was a fork
+    /// @return True if the update history forks at this update
     function isForkedAt(bytes32 didGenesisHash, bytes32 updateHash) external view returns (bool) {
         return (dids[didGenesisHash].updates[updateHash].countChildren > 1);
     }
 
-    // Returns the address of the verification method as of the update, or 0x0 if it has not been stored
+    /// @notice Return the verification method address as of the specific update, if it has been stored
+    /// @dev We do not always store the verification method address, so it may be 0x0 even though there is one
+    /// @param didGenesisHash The did
+    /// @param updateHash The update at which you want to know the verification address
+    /// @return The verification method address as of the specific update, if it has been stored
     function verificationAddressAt(bytes32 didGenesisHash, bytes32 updateHash) external view returns (address) {
         return dids[didGenesisHash].updates[updateHash].verificationMethod;
     }
 
-    // If your did history has forked, you can choose a fork by marking an update (you would normally choose a tip) legit.
-    // This function will tell you if that update, or one of its descendents, has forked.
+    /// @notice Return whether the history of the DID has forked between the specified update and the specified tip
+    /// @dev If your did history has forked, you can choose a fork by marking an update (you would normally choose a tip) legit.
+    /// @dev This function is designed to tell you if that update, or one of its descendents, has forked.
+    /// @param didGenesisHash The did you are updating
+    /// @param chosenOpHash The lowest update to consider from the tip
+    /// @param tip The tip of the branch you are interested in
+    /// @return True if the chain has forked between the chosenOpHash and the tip
     function isBranchForkedSince(bytes32 didGenesisHash, bytes32 chosenOpHash, bytes32 tip)
         public
         view
@@ -96,9 +105,13 @@ contract MightHaveDonePLCDirectory is DidVerifier {
         revert("chosenOpHash not found");
     }
 
-    // This will append an entry to the tree
-    // Usually parentHash will be the tip of the tree and we won't be adding a fork
-    // But you could be adding a child to something lower down the tree to make a fork
+    /// @notice Register a DID update
+    /// @dev This will append an entry to the tree
+    /// @dev Usually parentHash will be the tip of the tree and we will just be extending it
+    /// @dev But you could be adding a child to something lower down the tree to make a fork
+    /// @param didGenesisHash The did you are updating
+    /// @param newHash The hash of the entry you are adding
+    /// @param parentHash The hash of the entry you are appending to
     function registerUpdate(bytes32 didGenesisHash, bytes32 newHash, bytes32 parentHash) internal {
         require(parentHash != bytes32(0), "non-genesis operation must have a parent hash");
         require(dids[didGenesisHash].updates[parentHash].recordedTimestamp > 0, "parentHash not found in chain");
@@ -122,9 +135,11 @@ contract MightHaveDonePLCDirectory is DidVerifier {
         emit LogRegisterUpdate(didGenesisHash, parentHash, newHash);
     }
 
-    // NB You can pass any content you like here and we'll make a DID record for its hash
-    // If you want to make a did entry for a picture of your cat that's fine, go right ahead
-    // We only care what's in the record if you try to update it
+    /// @notice Register a DID record and an entry for the first (genesis) update
+    /// @param entryHash The hash of the first entry which creates the DID ID
+    /// @dev NB You can pass any content you like here and we'll make a DID record for its hash
+    /// @dev If you want to make a did entry for a picture of your cat that's fine, go right ahead
+    /// @dev We only care what's in the record if you try to query or update it
     function registerGenesis(bytes32 entryHash) internal {
         dids[entryHash].didBytes = bytes(abi.encode(entryHash));
         dids[entryHash].updates[entryHash].recordedTimestamp = uint128(block.timestamp);
@@ -206,6 +221,11 @@ contract MightHaveDonePLCDirectory is DidVerifier {
         }
     }
 
+    /// @notice Store the verification method address for an update
+    /// @param didGenesisHash The hash of the first entry which creates the DID ID
+    /// @param entryHash The hash of the first entry which creates the DID ID
+    /// @param entry The entry in which we should find the pubkey
+    /// @param pubkey The pubkey we expect to find, but as an uncompressed pubkey
     function _storeVerificationMethod(
         bytes32 didGenesisHash,
         bytes32 entryHash,
@@ -216,6 +236,12 @@ contract MightHaveDonePLCDirectory is DidVerifier {
         dids[didGenesisHash].updates[entryHash].verificationMethod = extractVerificationMethod(entry, pubkey);
     }
 
+    /// @notice Store the verification method address for an update which is otherwise already registered
+    /// @dev This is stored automatically by registerUpdates when adding to the tip.
+    /// @dev You only need it if you want to be able to read the address of an intermediate update for some reason.
+    /// @param didGenesisHash The hash of the first entry which creates the DID ID
+    /// @param entry The entry in which we should find the pubkey
+    /// @param pubkey The pubkey we expect to find, but as an uncompressed pubkey
     function storeVerificationMethod(bytes32 didGenesisHash, bytes calldata entry, bytes calldata pubkey) external {
         bytes32 entryHash = sha256(entry);
         _storeVerificationMethod(didGenesisHash, entryHash, entry, pubkey);
