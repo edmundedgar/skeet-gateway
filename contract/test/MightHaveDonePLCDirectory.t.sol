8cb5dbfdd2d14fd74cdf15aee80c35147183f742
Edmund Edgar <ed@socialminds.jp>
2025-02-17 16:34:38 +0900

    Test for multiple forking branches

--- contract/test/MightHaveDonePLCDirectory.t.sol
+++ contract/test/MightHaveDonePLCDirectory.t.sol
@@ -7,19 +7,14 @@ import {Vm} from "forge-std/Vm.sol";
 import {MightHaveDonePLCDirectory} from "../src/MightHaveDonePLCDirectory.sol";
 
 contract MightHaveDonePLCDirectoryTest is Test, DidProofLoader {
-    MightHaveDonePLCDirectory public repo;
-    bytes32 did;
-    bytes32 finalUpdateHash;
 
-    function setUp() public {
-        repo = new MightHaveDonePLCDirectory();
+    function testRegisterUpdates() public {
+        MightHaveDonePLCDirectory repo = new MightHaveDonePLCDirectory();
         DidProof memory proof = _loadProofFixture("did:plc:pyzlzqt6b2nyrha7smfry6rv.json");
-        finalUpdateHash = sha256(proof.ops[proof.ops.length - 1]);
-        did = bytes32(bytes(proof.did));
+        bytes32 finalUpdateHash = sha256(proof.ops[proof.ops.length - 1]);
         repo.registerUpdates(bytes32(0), proof.ops, proof.sigs, proof.pubkeys, proof.pubkeyIndexes);
-    }
+        bytes32 did = bytes32(bytes(proof.did));
 
-    function testRegisterUpdates() public view {
         // Output by our python script for the pubkey using eth_key when preparing the fixture
         address expectedFinalVerificationMethod = 0x96a0C856E027f207Af09BCeAe1E27D7AEA9043dB;
 
@@ -27,8 +22,66 @@ contract MightHaveDonePLCDirectoryTest is Test, DidProofLoader {
         assertEq(storedVerificationAddress, expectedFinalVerificationMethod, "Not expected address");
     }
 
-    function testUncontroversialTip() public view {
-        // TODO: Make some forking updates and test it gets zeroed
-        assertEq(finalUpdateHash, repo.uncontroversialTip(did), "The tip should be registered as uncontroversial");
+    function testRegisterSplitUpdates() public {
+        MightHaveDonePLCDirectory repo = new MightHaveDonePLCDirectory();
+
+        DidProof memory proof = _loadProofFixture("did:plc:pyzlzqt6b2nyrha7smfry6rv.0.json");
+        bytes32 did = bytes32(bytes(proof.did));
+        bytes32 finalUpdateHash = sha256(proof.ops[proof.ops.length - 1]);
+        repo.registerUpdates(bytes32(0), proof.ops, proof.sigs, proof.pubkeys, proof.pubkeyIndexes);
+        address storedVerificationAddress = repo.verificationAddressAt(did, finalUpdateHash);
+        assertNotEq(storedVerificationAddress, address(0), "Null verificationaddress");
+        assertEq(finalUpdateHash, repo.uncontroversialTip(did));
+
+        DidProof memory proof2 = _loadProofFixture("did:plc:pyzlzqt6b2nyrha7smfry6rv.01.json");
+        bytes32 finalUpdateHash2 = sha256(proof2.ops[proof2.ops.length - 1]);
+        assertEq(did, bytes32(bytes(proof2.did)), "did changed");
+        repo.registerUpdates(did, proof2.ops, proof2.sigs, proof2.pubkeys, proof2.pubkeyIndexes);
+        assertNotEq(finalUpdateHash, finalUpdateHash2);
+        assertEq(finalUpdateHash2, repo.uncontroversialTip(did));
+
+        DidProof memory proof3 = _loadProofFixture("did:plc:pyzlzqt6b2nyrha7smfry6rv.12.json");
+        bytes32 finalUpdateHash3 = sha256(proof3.ops[proof3.ops.length - 1]);
+        assertEq(did, bytes32(bytes(proof3.did)), "did changed");
+        repo.registerUpdates(did, proof3.ops, proof3.sigs, proof3.pubkeys, proof3.pubkeyIndexes);
+        assertNotEq(finalUpdateHash, finalUpdateHash3);
+        assertNotEq(finalUpdateHash2, finalUpdateHash3);
+        assertEq(finalUpdateHash3, repo.uncontroversialTip(did));
+    }
+
+    function testRegisterSplitForkingUpdates() public {
+        MightHaveDonePLCDirectory repo = new MightHaveDonePLCDirectory();
+
+        DidProof memory proof = _loadProofFixture("did:plc:ee7kjipyhx3cf6nmh2l5scbl.common.json");
+        bytes32 did = bytes32(bytes(proof.did));
+        bytes32 finalUpdateHash = sha256(proof.ops[proof.ops.length - 1]);
+        repo.registerUpdates(bytes32(0), proof.ops, proof.sigs, proof.pubkeys, proof.pubkeyIndexes);
+        address storedVerificationAddress = repo.verificationAddressAt(did, finalUpdateHash);
+        assertNotEq(storedVerificationAddress, address(0), "Null verificationaddress");
+        assertEq(finalUpdateHash, repo.uncontroversialTip(did));
+
+        DidProof memory proof2 = _loadProofFixture("did:plc:ee7kjipyhx3cf6nmh2l5scbl.fork1.json");
+        bytes32 finalUpdateHash2 = sha256(proof2.ops[proof2.ops.length - 1]);
+        assertEq(did, bytes32(bytes(proof2.did)), "did changed");
+        repo.registerUpdates(did, proof2.ops, proof2.sigs, proof2.pubkeys, proof2.pubkeyIndexes);
+        assertNotEq(finalUpdateHash, finalUpdateHash2);
+        assertEq(finalUpdateHash2, repo.uncontroversialTip(did));
+
+        DidProof memory proof3 = _loadProofFixture("did:plc:ee7kjipyhx3cf6nmh2l5scbl.fork2.json");
+        bytes32 finalUpdateHash3 = sha256(proof3.ops[proof3.ops.length - 1]);
+        assertEq(did, bytes32(bytes(proof3.did)), "did changed");
+        repo.registerUpdates(did, proof3.ops, proof3.sigs, proof3.pubkeys, proof3.pubkeyIndexes);
+        assertNotEq(finalUpdateHash, finalUpdateHash3);
+        assertNotEq(finalUpdateHash2, finalUpdateHash3);
+
+        assertEq(bytes32(0), repo.uncontroversialTip(did));
+    }
+
+    function testHomeMadeUpdates() public {
+        MightHaveDonePLCDirectory repo = new MightHaveDonePLCDirectory();
+        DidProof memory proof = _loadProofFixture("did:plc:ee7kjipyhx3cf6nmh2l5scbl.json");
+        bytes32 finalUpdateHash = sha256(proof.ops[proof.ops.length - 1]);
+        bytes32 did = bytes32(bytes(proof.did));
+        repo.registerUpdates(bytes32(0), proof.ops, proof.sigs, proof.pubkeys, proof.pubkeyIndexes);
     }
 }
