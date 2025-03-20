907b377fdaf8234d99b4ac5434ffea355d3f2537
Edmund Edgar <ed@socialminds.jp>
2025-03-20 18:11:21 +0800

    Stop hard-coding the gas limit, estimates seem to be working now (?)

--- python-tools/send_tx.py
+++ python-tools/send_tx.py
@@ -49,12 +49,11 @@ def sendTX(item):
             w3.to_bytes(hexstr=payload['commitNode']),
             w3.to_bytes(hexstr=payload['sig']),
         ).build_transaction({
-            "gas": 1000000,
             "from": ACCOUNT.address,
             "nonce": w3.eth.get_transaction_count(ACCOUNT.address),
         })
         gas = w3.eth.estimate_gas(tx)
-        print("gas is" + str(gas))
+        print("Gas estimate: " + str(gas))
     except web3.exceptions.ContractLogicError as err:
         return (False, err.message)
     signed_tx = w3.eth.account.sign_transaction(tx, private_key=ACCOUNT.key).raw_transaction
