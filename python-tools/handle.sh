6ebb05530256ec06f6707f074e4a08c0019398ce
Edmund Edgar <ed@socialminds.jp>
2025-03-25 14:35:37 +0800

    also do report

--- python-tools/handle.sh
+++ python-tools/handle.sh
@@ -6,8 +6,9 @@
 python fetch_skeets.py 
 python prepare_payload.py 
 python send_tx.py 
+python report_tx.py
 
 python find_active_dids.py 
 python watch_did_update.py 
 python prepare_did_update.py 
-python send_did_tx.py 
+python send_did_tx.py
