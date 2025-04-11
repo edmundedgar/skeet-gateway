f7e4f60c2879fd15618618a9575b088301443c6e
Edmund Edgar <ed@socialminds.jp>
2025-04-11 14:47:11 +0900

    Handle errors thrown by more recent web3.py

--- python-tools/send_tx.py
+++ python-tools/send_tx.py
@@ -54,7 +54,7 @@ def sendTX(item):
         })
         gas = w3.eth.estimate_gas(tx)
         print("Gas estimate: " + str(gas))
-    except web3.exceptions.ContractLogicError as err:
+    except (web3.exceptions.ContractLogicError, web3.exceptions.Web3RPCError) as err:
         return (False, None, err)
     signed_tx = w3.eth.account.sign_transaction(tx, private_key=ACCOUNT.key).raw_transaction
     tx_hash = w3.eth.send_raw_transaction(signed_tx)
@@ -112,7 +112,7 @@ def handleItem(item):
             item['x_tx_logs'].append(json_friendly_obj)
         skeet_queue.updateStatus(at_uri, bot, "tx", "report", item)
     else:
-        if err is not None and err.message == 'execution reverted: Already handled':
+        if err is not None and (err.message == 'execution reverted: Already handled' or err.message == "{'code': -32015, 'message': 'Already handled'}"):
             print("Was already completed: " + at_uri + " (" + bot + ")")
             skeet_queue.updateStatus(at_uri, bot, "tx", "report", item)
         else:
